7d6c7d8353ee29998d36eb8b78e1691af1123269
Trezalys <arthur.meslin@isen-ouest.yncrea.fr>
2018-06-01 19:45:11 +0200

    Ajout de la page d'inscription et de l'utilisateur

---
+++ register.php
@@ -0,0 +1,133 @@
+
+<?php?>
+
+<html>
+
+	<head>
+		<!-- Meta tags -->
+		<meta charset="utf-8" />
+		<meta name="author" content="Adrien LEBOURGEOIS - Arthur MESLIN" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Title -->
+		<title>Salade Quiz</title>
+
+		<!-- Css Styles -->
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+
+		<!-- Js Scripts -->
+		<script src="js/jquery.min.js" defer></script>
+		<script src="js/bootstrap.min.js" defer></script>
+
+		<script type="text/javascript" src="js/js.cookie.js" defer></script>
+	</head>
+
+
+
+
+
+	<body>
+		<div id="header">
+
+
+		</div>
+		<div class="row">
+			<div class="col-lg-1">
+			</div>
+			<h2 class =" col-lg-1 ">Inscription</h2>
+
+
+		</div>
+	</br>
+	</br>
+	</br>
+		<form>
+
+		<div class="container form-group" >
+			<div class="row">
+
+				<div class="col-lg-1">
+				</div>
+
+				<div class=" col-lg-3">
+					<label>Pseudonyme</label>
+					<input name="choix" type="text" placeholder="..." class="form-control"></input>
+				</div>
+
+				<div class="col-lg-3">
+				</div>
+
+				<div class="col-lg-3">
+					<label>test php dynamique</label>
+					<input name="choix" type="text" placeholder="..." class="form-control"></input>
+
+				</div>
+
+			</div>
+
+			</br>
+
+			<div class="row">
+				<div class="col-lg-8">
+				</div>
+				<div >
+					<label>Afficher le mot de passe    </label>
+				</div>
+				<div class=" form-check  col-lg-1">
+					<input id="show-pwd" type="checkbox" class="form-check-input "></input>
+				</div>
+			</div>
+
+			</br>
+
+			<div class="row">
+
+				<div class="col-lg-1">
+				</div>
+
+				<div class=" col-lg-3">
+					<label>Email</label>
+					<input name="choix" type="email" placeholder="..." class="form-control" ></input>
+
+				</div>
+
+				<div class="col-lg-3">
+				</div>
+
+				<div class="col-lg-3">
+					<label>Confirmer le mot de passe</label>
+					<input name="choix" type="text" placeholder="..." class="form-control"></input>
+				</div>
+
+			</div>
+		</br>
+
+			<div class="row">
+
+				<div class="col-lg-3">
+				</div>
+				<div class="col-lg-4">
+					<button type="button" onclick="window.location='index.html'"class="btn btn-primary">Retour</button>
+				</div>
+				<div class=" col-lg-4">
+					<button type="submit" class="btn btn-primary">Valider</button>
+				</div>
+			</div>
+	</div>
+</form>
+
+
+
+
+
+	<div id="footer">
+
+	</div>
+
+	</body>
+
+
+
+</html>
---
+++ user.php
@@ -0,0 +1,272 @@
+
+<?php?>
+
+<html>
+
+	<head>
+		<!-- Meta tags -->
+		<meta charset="utf-8" />
+		<meta name="author" content="Adrien LEBOURGEOIS - Arthur MESLIN" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Title -->
+		<title>Salade Quiz</title>
+
+		<!-- Css Styles -->
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+
+		<!-- Js Scripts -->
+		<script src="js/jquery.min.js" defer></script>
+		<script src="js/bootstrap.min.js" defer></script>
+
+		<script type="text/javascript" src="js/js.cookie.js" defer></script>
+	</head>
+
+
+
+
+
+	<body>
+		<div id="header">
+
+		</div>
+
+    <div class="row">
+      <div class="col-lg-1">
+      </div>
+      <h4 class =" col-lg-2 " >Meilleurs scores</h4>
+
+      <div class="col-lg-2">
+      </div>
+      <h4 class =" col-lg-2 " >Mon historique</h4>
+
+
+
+    </div>
+  	</br>
+
+		<form>
+
+
+
+		<div class="container form-group" >
+
+
+      <!--Première section-->
+
+			<div class="row">
+
+
+				<div class="col-lg-1">
+				</div>
+
+				<div class=" col-lg-2"> <!-- Palmares-->
+          <div class="row">
+            <div>
+              <a id="id-user">User</a><a> | </a> <a id="score">score</a>
+              </br>
+              <a id="id-game-1">Partie 44100</a>
+              </br>
+              <button type="submit" class="btn btn-primary">Jouer</button>
+            </div>
+          </div>
+        </div>
+
+				<div class="col-lg-2">
+				</div>
+
+        <div class=" col-lg-2">
+          <div class="row">
+            <div>
+              <a id="score" >score</a>
+              </br>
+              <a id="id-game">Partie 44100</a>
+              </br>
+              <button type="submit" class="btn btn-primary">Jouer</button>
+            </div>
+          </div>
+        </div>
+			</div>
+
+
+
+    </br>
+
+<!--Deuxième section-->
+
+      <div class="row">
+
+        <div class="col-lg-1">
+        </div>
+
+        <div class=" col-lg-2"> <!-- Palmares-->
+          <div class="row">
+            <div>
+              <a id="id-user">User</a><a> | </a> <a id="score">score</a>
+              </br>
+              <a id="id-game-2">Partie 44100</a>
+              </br>
+              <button type="submit" class="btn btn-primary">Jouer</button>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-2">
+        </div>
+
+        <div class=" col-lg-2">
+          <div class="row">
+            <div>
+              <a id="score">score</a>
+              </br>
+              <a id="id-game">Partie 44100</a>
+              </br>
+              <button type="submit" class="btn btn-primary">Jouer</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-2">
+        </div>
+        <h4 class =" col-lg-2 ">Générer une nouvelle partie</h4>
+      </div>
+
+
+    </br>
+
+    <!--Troisième section-->
+
+
+			<div class="row">
+
+				<div class="col-lg-1">
+				</div>
+
+        <div class=" col-lg-2"> <!-- Palmares-->
+          <div class="row">
+            <div>
+              <a id="id-user">User</a><a> | </a> <a id="score">score</a>
+              </br>
+              <a id="id-game-3">Partie 44100</a>
+              </br>
+              <button type="submit" class="btn btn-primary">Jouer</button>
+            </div>
+          </div>
+        </div>
+
+				<div class="col-lg-2">
+				</div>
+
+        <div class=" col-lg-2">
+          <div class="row">
+            <div>
+              <a id="score">score</a>
+              </br>
+              <a id="id-game">Partie 44100</a>
+              </br>
+              <button type="submit" class="btn btn-primary">Jouer</button>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-lg-2">
+        </div>
+        <button type="submit" class="btn btn-primary">C'est parti!</button>
+			</div>
+
+
+    </br>
+    <!--Quatrième section-->
+
+
+    <div class="row">
+
+      <div class="col-lg-1">
+      </div>
+
+      <div class=" col-lg-2"> <!-- Palmares-->
+        <div class="row">
+          <div>
+            <a id="id-user">User</a><a> | </a> <a id="score">score</a>
+            </br>
+            <a id="id-game-4">Partie 44100</a>
+            </br>
+            <button type="submit" class="btn btn-primary">Jouer</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-2">
+      </div>
+
+      <div class=" col-lg-2">
+        <div class="row">
+          <div>
+            <a id="score">score</a>
+            </br>
+            <a id="id-game">Partie 44100</a>
+            </br>
+            <button type="submit" class="btn btn-primary">Jouer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+</br>
+
+<!--Cinquième section-->
+
+
+    <div class="row">
+
+      <div class="col-lg-1">
+      </div>
+
+      <div class=" col-lg-2"> <!-- Palmares-->
+        <div class="row">
+          <div>
+            <a id="id-user">User</a><a> | </a> <a id="score">score</a>
+            </br>
+            <a id="id-partie-5">Partie 44100</a>
+            </br>
+            <button type="submit" class="btn btn-primary">Jouer</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-2">
+      </div>
+
+      <div class=" col-lg-2">
+        <div class="row">
+          <div>
+            <a id="score">score</a>
+            </br>
+            <a id="id-partie">Partie 44100</a>
+            </br>
+            <button type="submit" class="btn btn-primary">Jouer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+	</div>
+</form>
+
+
+
+
+
+	<div id="footer">
+
+	</div>
+
+	</body>
+
+
+
+</html>

5f5b8c0bff379b728a8e2c29b5321c6c4ae70190
alebou21 <adrien.lebourgeois@protonmail.com>
2018-06-13 16:16:25 +0200

    i

--- php/includes/Constants.php
+++ php/includes/Constants.php
@@ -1,25 +1,16 @@
 <?php
 
-
+define('MYSQL_SERVER_IP', "127.0.0.1");
+define('MYSQL_SERVER_PORT', "3306");
+define('MYSQL_DB_NAME', "saladeQuiz");
+define('MYSQL_DB_USER', 'saladeQuiz');
+define('MYSQL_DB_PASSWORD','saladeQuiz');
 
 
 define('QUESTION_NUMBER', 3);// on définit ici la constante permettant de réguler la quantité de questions dans une partie
 
 define('PROPOSITION_NUMBER', 3);// on définit ici la constante permettant de réguler la quantité de propositions affectées à une question dans une partie
 
-
 define('TOTAL_PROPOSITIONS', QUESTION_NUMBER*PROPOSITION_NUMBER);// on définit ici la constante permettant d'obtenir, avec un code plus léger, le nombre total de propositions
-                                                                                                                                                          //durant la partie
-
-
-
-
-
-
-
-
-
-
-
 
  ?>
